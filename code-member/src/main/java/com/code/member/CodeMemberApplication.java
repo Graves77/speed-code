19c713fc926984dbaa64cebc79bd338aa7aa114f
Graves77 <870519251@qq.com>
2022-11-30 22:51:47 +0800

    11.30 项目初始化

---
+++ code-member/src/main/java/com/code/member/CodeMemberApplication.java
@@ -0,0 +1,13 @@
+package com.code.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMemberApplication.class, args);
+    }
+
+}
---
+++ code-member/src/test/java/com/code/member/CodeMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
